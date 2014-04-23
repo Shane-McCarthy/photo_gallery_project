29344b5801d1b70a7b886cfe5b12d67c52197a56
Shane McCarthy <rx@doctorphong.com>
2014-04-23 08:34:16 -0700

    Update config.php

--- photo_gallery/includes/config.php
+++ photo_gallery/includes/config.php
@@ -7,8 +7,8 @@
  */
 // Database Constants
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "phong_admin");
-defined('DB_PASS')   ? null : define("DB_PASS", "stroker1");
+defined('DB_USER')   ? null : define("DB_USER", "");
+defined('DB_PASS')   ? null : define("DB_PASS", "");
 defined('DB_NAME')   ? null : define("DB_NAME", "photo_gallery");
 
 ?>
